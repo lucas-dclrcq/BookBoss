e31ae149ff2cb4c18b94b64cb8794c67ef0bcabb
Scotte Zinn <scotte@zinn.ca>
2026-03-08 19:56:38 -0400

    refactor(core): Sort capabilities

--- crates/core/src/types/capability.rs
+++ crates/core/src/types/capability.rs
@@ -6,23 +6,23 @@ pub type Capabilities = HashSet<Capability>;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum Capability {
-    SuperAdmin,
     Admin,
     ApproveImports,
     ConvertBook,
     DeleteBook,
     EditBook,
+    SuperAdmin,
 }
 
 impl Capability {
     pub fn as_str(&self) -> &'static str {
         match self {
-            Self::SuperAdmin => "SuperAdmin",
             Self::Admin => "Admin",
             Self::ApproveImports => "ApproveImports",
             Self::ConvertBook => "ConvertBook",
             Self::DeleteBook => "DeleteBook",
             Self::EditBook => "EditBook",
+            Self::SuperAdmin => "SuperAdmin",
         }
     }
 }
