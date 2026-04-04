4ed856ab04817828e727048fa098ebe617f4233f
Scotte Zinn <scotte@zinn.ca>
2026-04-04 11:12:07 -0400

    fix(database): migrations don't need a down

--- crates/database/src/migrations/m20260404_000040_create_koreader_document_hashes_table.rs
+++ crates/database/src/migrations/m20260404_000040_create_koreader_document_hashes_table.rs
@@ -57,8 +57,8 @@ impl MigrationTrait for Migration {
         Ok(())
     }
 
-    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager.drop_table(Table::drop().table(KoreaderDocumentHashes::Table).to_owned()).await
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
     }
 }
 
